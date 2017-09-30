2d2f21d286cf0b4af722b0a1ef4ecd7a74e59240
Michela Zucca <michela.zucca@heig-vd.ch>
2017-09-30 15:30:47 +0200

    Commentaire du code agent.js

--- src/agent.js
+++ src/agent.js
@@ -5,23 +5,35 @@ class Agent{
         this.credentials = credentials;
     }
 
+    /* Requête a appeler pour lancer la construction du tableau des données
+     * owner : nom de l'entreprise / utilisateur
+     * repo : dossier git
+     * searchType : type de la requetes, exemple les pullsrequest, les issues...
+    */
     fetchAndProcessAllPullRequests(owner, repo, searchType, AllPullRequestsAreAvailable){
+        // construction de l'url
         const targetUrl =  `https://api.github.com/repos/${owner}/${repo}/${searchType}?state=all`
+        // tableau des résultats
         let pullRequests = [];
+        // fonction utilisé en récursivité pour récupérer le contenu de toutes les pages et le concaténer
         function fetchAndProcessPage(pageUrl, credentials){
             console.log(`Fetching ${pageUrl}`);
+            // requête a envoyer
             request
                 .get(pageUrl)
                 .auth(credentials.username, credentials.token)
                 .end((err, res) => {
                     pullRequests = pullRequests.concat(res.body);
                     if(res.links.next){
+                        // lancé l'appel récursif pour la prochaine page
                         fetchAndProcessPage(res.links.next,credentials);
                     } else {
+                        // fin de la récursion
                         AllPullRequestsAreAvailable(null,pullRequests);
                     }
                 });
         }
+        // première appel à la fonction de récursion
         fetchAndProcessPage(targetUrl,this.credentials);
     }
 }
