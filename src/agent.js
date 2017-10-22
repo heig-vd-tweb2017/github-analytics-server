94cdcf4f2691aab26a3b121337a0f63205a70f32
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-10-22 16:16:22 +0200

    Opened and closed issues by dates are now correctly retreived

--- src/agent.js
+++ src/agent.js
@@ -12,12 +12,15 @@ class Agent {
   }
 
   /**
-   * Process the GitHub's API pages.
-   * @param {string} targetUrl The GitHub's API URL.
+   * Get all the opened issues.
+   * @param {string} owner The GitHub's owner of the repository
+   * @param {string} repo The repository.
    * @param {function} dataAreAvailable The function to call when data are available.
    * @param {function} endOfData The function to call when there are no more data.
    */
-  processPage(targetUrl, dataAreAvailable, endOfData) {
+  getOpenedIssues(owner, repo, dataAreAvailable, endOfData) {
+    const targetUrl = `https://api.github.com/repos/${owner}/${repo}/issues?state=all`;
+
     const dates = new Map();
     const users = new Map();
 
@@ -34,20 +37,22 @@ class Agent {
         .end((err, res) => {
           if (err == null) {
             res.body.forEach((record) => {
+              const openedDate = moment(record.created_at).format('YYYY-MM-DD');
+
+              // Store the number of opened issues at the given date
+              if (!dates.has(openedDate)) {
+                dates.set(openedDate, 1);
+              } else {
+                dates.set(openedDate, dates.get(openedDate) + 1);
+              }
+
+              // Save the user who opened the issue
               const user = record.user.login;
 
               if (!users.has(user)) {
                 users.set(user, 1);
               } else {
                 users.set(user, users.get(user) + 1);
-              }
-
-              const date = moment(record.created_at).format('YYYY-MM-DD');
-
-              if (!dates.has(date)) {
-                dates.set(date, 1);
-              } else {
-                dates.set(date, dates.get(date) + 1);
               }
             });
 
@@ -66,19 +71,6 @@ class Agent {
   }
 
   /**
-   * Get all the opened issues.
-   * @param {string} owner The GitHub's owner of the repository
-   * @param {string} repo The repository.
-   * @param {function} dataAreAvailable The function to call when data are available.
-   * @param {function} endOfData The function to call when there are no more data.
-   */
-  getOpenedIssues(owner, repo, dataAreAvailable, endOfData) {
-    const targetUrl = `https://api.github.com/repos/${owner}/${repo}/issues?state=open`;
-
-    this.processPage(targetUrl, dataAreAvailable, endOfData);
-  }
-
-  /**
    * Get all the closed issues.
    * @param {string} owner The GitHub's owner of the repository
    * @param {string} repo The repository.
@@ -86,9 +78,66 @@ class Agent {
    * @param {function} endOfData The function to call when there are no more data.
    */
   getClosedIssues(owner, repo, dataAreAvailable, endOfData) {
-    const targetUrl = `https://api.github.com/repos/${owner}/${repo}/issues?state=closed`;
+    const targetUrl = `https://api.github.com/repos/${owner}/${repo}/issues?state=all`;
 
-    this.processPage(targetUrl, dataAreAvailable, endOfData);
+    const dates = new Map();
+    const users = new Map();
+
+    /**
+     * Function called until all the data are fetched.
+     * @param {string} pageUrl The GitHub's API URL.
+     * @param {JSON object with username and token} credentials  The credentials to use
+     * to query GitHub.
+     */
+    function fetchAndProcessPage(pageUrl, credentials) {
+      request
+        .get(pageUrl)
+        .auth(credentials.username, credentials.token)
+        .end((err, res) => {
+          if (err == null) {
+            res.body.forEach((record) => {
+              const openedDate = moment(record.created_at).format('YYYY-MM-DD');
+
+              // Set zero closed issue at the given opened date
+              if (!dates.has(openedDate)) {
+                dates.set(openedDate, 0);
+              }
+
+              // If there is a closed date, we add to the
+              // closed issue at the given closed date
+              if (record.closed_at !== null) {
+                const closedDate = moment(record.closed_at).format('YYYY-MM-DD');
+
+                // Store the number of closed issues at the given date
+                if (!dates.has(closedDate)) {
+                  dates.set(closedDate, 1);
+                } else {
+                  dates.set(closedDate, dates.get(closedDate) + 1);
+                }
+
+                // Save the user who closed the issue
+                const user = record.user.login;
+
+                if (!users.has(user)) {
+                  users.set(user, 1);
+                } else {
+                  users.set(user, users.get(user) + 1);
+                }
+              }
+            });
+
+            dataAreAvailable(null, { users, dates });
+
+            if (res.links.next) {
+              fetchAndProcessPage(res.links.next, credentials);
+            } else {
+              endOfData();
+            }
+          }
+        });
+    }
+
+    fetchAndProcessPage(targetUrl, this.credentials);
   }
 }
 
